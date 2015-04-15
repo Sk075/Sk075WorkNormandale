222e55e221eef2a37bac7694f54df0012be12802
Sk075 <skitoy2010@gmail.com>
2015-04-15 16:42:39 -0500

    master

---
+++ Database Chapter 9 Work 2.sql
@@ -0,0 +1,37 @@
+a) 
+1. Interview the shop manager
+2. Interview the mechanics
+3. Obtain a general description of company operations
+4. Create a description of each system process
+5. Create a conceptual model using ER diagrams
+6. Draw a data flow diagram and system flowcharts
+7. Normalize the conceptual model
+8. Create the file (table) structures
+9. Load the database
+10. Create the application programs
+11. Test the system.
+b) The modules that should be Inventory, Payroll, Work Order, and Customer. Payroll modules deal with the employee and payroll information. Work Order modules deal with car maintenance history check and work orders for maintenances on the car. Customer modules deal with tracking of bill orders from customers and customer’s payments. Inventory modules deal with sub-module of things including parts and purchasing. 
+Source: http://www.docstoc.com/docs/99850807/Problem-Set-2-Solutions---Chapter-9 
+c) Data Dictionary is helpful for database so we can be able to put the needed data and avoid repetition.
+Here are some ways to look at it. 
+Define entities, attributes, primary keys, and foreign keys. (The foreign keys serve as the basis for the relationships among the entities).
+Make decisions about adding new primary key attributes to satisfy end-user and processing requirements. 
+Make decisions about the treatment of composite and multivalued attributes. 
+Making decisions about adding derived attributes to satisfy processing requirements. 
+Make decisions about placement of foreign keys in 1:1 relationships
+Avoid unnecessary ternary relationships 
+Draw the corresponding ER Diagram
+Normalize the entities 
+Include all data element definitions in the data dictionary 
+Make decisions about standard naming conventions 
+d) Job of the designer to provide solutions to main problems found during the initial stage of the project with external and internal constraints. Like for example, we can assume safely that the owner of ABC Car Service and Repair Center has a time frame in mind with limitation of spending. As is true in all design work, the designer and the manager must be one accord concerning modules prioritization and yields of creating the most beneficial within stated time and development budget constraints. 
+Keep in mind that it’s always useful to develop a modular system that provides for future enhancement and expansion. Like how to the ABC Car Service and Repair management decides to integrate all of its service stations in the state in order to provide better statewide service. These types of integrations may yield many benefits: The car history of each car will be available to any car station which cars have more around more than a location; inventory of parts will be on-line which allows part orders to be placed between stations; mechanics can better be able to share tips on mechanic problems and other situations. 
+Source: http://www.docstoc.com/docs/99850807/Problem-Set-2-Solutions---Chapter-9 
+e) The best way to conceptual design is centralized design. This is due to that the fact only one designer will be doing this database. But since the database can be complicated and may need to accommodate changes, the top-down approach is useful. 
+Source: http://www.docstoc.com/docs/99850807/Problem-Set-2-Solutions---Chapter-9 
+e) R1: A Monthly Activity Sheet which service categories summaries by branch and month all together. These reports will help become the basis of forecasting personnel and stock requirements for each branch and period as well. 
+R2: A Mechanic Summary Report which contains a summary of work hours clocked by mechanics. This report would be done weekly and be useful for payroll and maintaining personnel scheduling purposes. 
+R3: An monthly report which contains a summary of parts in inventory, at what level to reorder parts and information at vendors who will provide the parts to be recorded. This report will be useful for inventory management purposes. 
+R4: A Customer Activity Report which contains a breakdown in customers and their spending habits. This is good for promotional purposes, reminding the customers of deals and keeping 
+Source: 
+http://www.docstoc.com/docs/99850807/Problem-Set-2-Solutions---Chapter-9 
